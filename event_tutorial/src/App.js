553d0909aa68454b23364db1a2d6ac42c618db71
Seoheeda <1015aliciay@gmail.com>
2024-03-16 16:47:04 +0900

    12th tutorial

---
+++ event_tutorial/src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	function handleClick() {
+		setCount(count + 1);
+	}
+
+  return (
+    <div>
+      <button onClick={handleClick}>버튼</button>
+      <div>버튼을 {count}번 눌렀습니다!</div>
+    </div>
+  );
+}
+
+export default App;
