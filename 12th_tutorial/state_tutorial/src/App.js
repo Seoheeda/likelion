9a9f90b5b3e7eca22333e876436e1e50aaf60c52
Seoheeda <1015aliciay@gmail.com>
2024-05-26 21:21:58 +0900

    로그인 세션 자료

--- 12th_tutorial/state_tutorial/src/App.js
+++ 12th_tutorial/state_tutorial/src/App.js
@@ -1,11 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import axios from "axios";
+
+import { Cookies } from "react-cookie";
+
+const cookies = new Cookies();
+
+export const setCookie = (name, value) => {
+  return cookies.set(name, value);
+};
+export const getCookie = (name) => {
+  return cookies.get(name);
+};
 
 function App() {
-	const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-	function handleClick() {
-		setCount(count + 1);
-	}
+  const apiCall = axios.create({
+    baseURL: "https://dummyjson.com",
+  });
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  
+  const login = async (id="emilys", pw="emilyspass") => {
+    try {
+      const response = await apiCall.post("/auth/login", {
+        username: id,
+        password: pw,
+      });
+      setCookie("token", response.data.token);
+      return response.data.token;
+    } catch (error) {
+      return error;
+    }
+  };
+
+  console.log(login());
+  console.log("토큰", localStorage.getItem("token"));
 
   return (
     <div>
